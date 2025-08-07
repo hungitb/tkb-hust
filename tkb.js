c340bb1afe96ec3a6a0c4f8145afa9b6b7dd9d45
hung-hust <122149751+hung-itb@users.noreply.github.com>
2025-08-07 21:09:09 +0700

    update

--- tkb.js
+++ tkb.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";window.db39="THỜI KHÓA BIỂU DỰ KIẾN KỲ 20251 (Cập nhật 04.08.2025)"})();
+(()=>{"use strict";window.db39="THỜI KHÓA BIỂU DỰ KIẾN KỲ 20242 (Cập nhật 07.08.2025)"})();
